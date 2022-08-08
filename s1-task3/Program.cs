073dbee4597a85eb4a8ce0c6cbc0b7564460d60e
Anna.K <kkk.aanya1996@mail.ru>
2022-08-08 20:00:13 +0300

    разбивка по папкам

---
+++ s1-task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 2: напишите программу, которая будет выдавать название дня недели 
+по заданному номеру:
+3-> среда
+5-> пятница
+*/
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1) Console.WriteLine("Monday");
+else if (day == 2) Console.WriteLine("Tuesday");
+else if (day == 3) Console.WriteLine("Wednesday");
+else if (day == 4) Console.WriteLine("Thursday");
+else if (day == 5) Console.WriteLine("Friday");
+else if (day == 6) Console.WriteLine("Saturday");
+else if (day == 7) Console.WriteLine("Sunday");
+else if(day <= 0 || day > 7) Console.WriteLine("Нет такого дня недели!");
+
+/* мне дз подумать над выводом через массив int day = {Monday[1]..., Tuesday, Wednesday,
+ Thursday, Friday, Saturday, Sunday}*/
+ 
+
---
+++ sem1-task7/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 7: напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает последнюю цифру этого числа. Пример:
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+Console.WriteLine("Введите трехзначное число: ");
+int userNumb1 = Convert.ToInt32(Console.ReadLine());
+int userNumb2 = userNumb1%10;
+Console.WriteLine(userNumb2);
