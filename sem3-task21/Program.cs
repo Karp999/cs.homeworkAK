e4c5ca50059e1b56e50778edc8fc77e3cdc9304c
Anna.K <kkk.aanya1996@mail.ru>
2022-08-11 21:25:12 +0300

    sem3-task21

---
+++ sem3-task21/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 21: Напишите программу, которая принимает на вход координаты двух точек и
+ находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/
+Console.WriteLine("Введите X1: ");
+int userX1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X2: ");
+int userX2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y1: ");
+int userY1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y2: ");
+int userY2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt((userX2-userX1)*(userX2-userX1)+(userY2-userY1)*(userY2-userY1));
+distance = Math.Round(distance, 2);
+Console.WriteLine($"Расстояние равно: {distance}");
+
+
+
+
