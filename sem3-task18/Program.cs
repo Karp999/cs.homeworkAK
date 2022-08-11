3c01d23cc9d84db2232e265ffa5651230c890153
Anna.K <kkk.aanya1996@mail.ru>
2022-08-11 21:03:53 +0300

    sem3-task18

---
+++ sem3-task18/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 18: Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+void getCoordinatefromQuoter (int quoter)
+{
+    if (quoter == 1)
+    {
+        Console.WriteLine($"x>0 и y>0");
+    }
+    else if (quoter == 2)
+    {
+        Console.WriteLine($"x<0 и y>0");
+    }
+     else if (quoter == 3)
+    {
+        Console.WriteLine($"x<0 и y<0");
+    }
+     else if (quoter == 4)
+    {
+        Console.WriteLine($"x>0 и y<0");
+    }
+    else 
+    {
+         Console.WriteLine($"ОШИБКА: количество четвертей всего четыре!!!");
+    }
+}
+Console.WriteLine("Введите четверть:");
+int userQuoter = Convert.ToInt32(Console.ReadLine());
+
+getCoordinatefromQuoter (userQuoter);
