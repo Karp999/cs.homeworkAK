99620ffc99beb40f15eebc62f06cfcf3c357b19e
Anna.K <kkk.aanya1996@mail.ru>
2022-08-14 21:34:25 +0300

    homework.task10

---
+++ homework.task10/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+показывает вторую цифру этого числа. Пример:
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.WriteLine("Введите трехзначное число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int secondANDthirdNumber = userNumber%100; 
+int thirdNumber = userNumber%10;
+int secondNumber = ((userNumber%100-userNumber%10)/10);
+
+Console.WriteLine(secondNumber);
+
