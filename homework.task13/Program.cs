428b6b1dc7126d92263cf711664133a324edaffe
Anna.K <kkk.aanya1996@mail.ru>
2022-08-14 22:18:43 +0300

    homework.task13

--- homework.task13/Program.cs
+++ homework.task13/Program.cs
@@ -4,3 +4,18 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber > 99)
+{
+    while (userNumber > 999)
+    {
+        userNumber = userNumber / 10;
+    }
+   Console.WriteLine(userNumber % 10);
+}
+else
+{
+    Console.WriteLine("В заданном числе нет третьей цифры.");
+}
